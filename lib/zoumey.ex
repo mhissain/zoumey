97be85413807e10b082a1cbeb3d09a276e5c3fe9
Kara Taylor <ktaylor.rn@outlook.com>
2023-03-04 21:47:32 -0500

    initial commit

---
+++ lib/zoumey.ex
@@ -0,0 +1,18 @@
+defmodule Zoumey do
+  @moduledoc """
+  Documentation for `Zoumey`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zoumey.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zoumey_test.exs
@@ -0,0 +1,8 @@
+defmodule ZoumeyTest do
+  use ExUnit.Case
+  doctest Zoumey
+
+  test "greets the world" do
+    assert Zoumey.hello() == :world
+  end
+end
